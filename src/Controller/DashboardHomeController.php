cad39bbc143ec0c43e4aaed6699ee3a93d7ad75d
Carlos Herrera <carherco@gmail.com>
2022-05-14 12:40:05 +0200

    se muestra tipo y mensaje de error

--- src/Controller/DashboardHomeController.php
+++ src/Controller/DashboardHomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\TestCase;
+use App\Repository\TestCaseRepository;
 use App\Repository\TestReportRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,12 +12,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardHomeController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard_home')]
-    public function index(TestReportRepository $reportRepository): Response
+    public function index(TestReportRepository $reportRepository, TestCaseRepository $testCaseRepository): Response
     {
         //$lastReport =  $reportRepository->findLastReport();
         $lastReport =  $reportRepository->find(1);
+        $testCasesWithErrors = $testCaseRepository->findByStatus(TestCase::ERROR);
+        $testCasesWithFailures = $testCaseRepository->findByStatus(TestCase::Failed);
+
         $output = [
-            'report' => $lastReport
+            'report' => $lastReport,
+            'testCasesWithErrorsOrFailures' => array_merge($testCasesWithErrors, $testCasesWithFailures)
         ];
 
         return $this->render('dashboard_home/index.html.twig', $output);
